e6d91d2d393bba0947125072301ea0fc549eafaf
Vengal Reddy Sandeep <vengalreddysandeep@gmail.com>
2024-07-01 18:26:56 +0530

    Done

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,12 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> s=new HashSet();
+        for(int i=0;i<nums.length;i++){
+        if(!s.add(nums[i])){
+            return true;
+        }}
+
         return false;
     }
 }
